29e25d5351296b4dea6dc5dc5b3ec85f3afc7060
kymokim <kymokim@gmail.com>
2023-09-30 19:23:30 +0900

    First commit

---
+++ src/test/java/com/example/Fooding/FoodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fooding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

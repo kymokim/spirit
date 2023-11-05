a0a97bffd3d0762276ef844fc5be62570d84471a
kymokim <kymokim@gmail.com>
2023-11-06 03:46:00 +0900

    [Fix] Menu attribute, method refactored.

--- src/main/java/com/example/Fooding/menu/service/MenuService.java
+++ src/main/java/com/example/Fooding/menu/service/MenuService.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class MenuService {
     private final MenuRepository menuRepository;
     private final StoreRepository storeRepository;
+
     public void createMenu(RequestMenu.CreateMenuDto createMenuDto) {
         Store store = storeRepository.findById(createMenuDto.getStoreId()).get();
         if(store == null) {
@@ -29,10 +30,10 @@ public class MenuService {
     }
 
     public List<ResponseMenu.GetAllMenuDto> getAllMenu() {
-        List<Menu> tasks = menuRepository.findAll();
-        List<ResponseMenu.GetAllMenuDto> list = new ArrayList<>();
-        tasks.stream().forEach(task -> list.add(ResponseMenu.GetAllMenuDto.toDto(task)));
-        return list;
+        List<Menu> entityList = menuRepository.findAll();
+        List<ResponseMenu.GetAllMenuDto> dtoList = new ArrayList<>();
+        entityList.stream().forEach(menu -> dtoList.add(ResponseMenu.GetAllMenuDto.toDto(menu)));
+        return dtoList;
     }
 
     public ResponseMenu.GetMenuDto getMenu(Long id) {
@@ -40,16 +41,8 @@ public class MenuService {
         return ResponseMenu.GetMenuDto.toDto(menu);
     }
 
-    public List<ResponseMenu.storeIdMenuDto> getById(Long storeId) {
-        //List<Menu> menus = menuRepository.findAllByStoreId(storeId);
-        List<Menu> menus = menuRepository.findAll();
-        List<ResponseMenu.storeIdMenuDto> dtoList = new ArrayList<>();
-        menus.stream().forEach(menu -> dtoList.add(ResponseMenu.storeIdMenuDto.toDto(menu)));
-        return dtoList;
-    }
-
     public void updateMenu(RequestMenu.UpdateMenuDto updateMenuDto) {
-        Menu originalMenu = menuRepository.findById(updateMenuDto.getId()).get();
+        Menu originalMenu = menuRepository.findById(updateMenuDto.getMenuId()).get();
         Menu updatedMenu = RequestMenu.UpdateMenuDto.toEntity(originalMenu, updateMenuDto);
         menuRepository.save(updatedMenu);
     }

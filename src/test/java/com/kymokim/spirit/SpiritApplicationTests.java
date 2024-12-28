771fcedf396ede241f96a2d1279f969152fdd7a1
kymokim <kymokim@gmail.com>
2024-12-28 21:04:14 +0900

    fix : test s3 config changed to mock.

--- src/test/java/com/kymokim/spirit/SpiritApplicationTests.java
+++ src/test/java/com/kymokim/spirit/SpiritApplicationTests.java
@@ -2,10 +2,12 @@ package com.kymokim.spirit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Import(S3MockConfig.class)
 class SpiritApplicationTests {
 
 	@Test

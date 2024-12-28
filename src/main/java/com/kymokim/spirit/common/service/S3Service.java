5f81756c97011471676fe15321cd3a788df28bf2
kymokim <kymokim@gmail.com>
2024-12-28 20:43:07 +0900

    refactor : aws sdk version, s3 code changed.

--- src/main/java/com/kymokim/spirit/common/service/S3Service.java
+++ src/main/java/com/kymokim/spirit/common/service/S3Service.java
@@ -1,18 +1,17 @@
 package com.kymokim.spirit.common.service;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,58 +19,66 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @PropertySource("classpath:/secret/application-s3.properties")
 public class S3Service {
-    private final AmazonS3Client amazonS3Client;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
     // S3 업로드
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        System.out.println(multipartFile.getName());
-        System.out.println(multipartFile.getOriginalFilename());
-        //S3에 Multipartfile 타입은 전송이 안되므로 file로 타입 전환
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File convert failed"));
         return upload(uploadFile, dirName);
     }
-    private String upload(File uploadFile, String dirName){
-        String fileName = dirName + "/"+ UUID.randomUUID().toString()+ uploadFile.getName();
+
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + UUID.randomUUID().toString() + uploadFile.getName();
         String uploadImageUrl = putS3(uploadFile, fileName);
-        System.out.println(uploadImageUrl);
         removeNewFile(uploadFile);
         return uploadImageUrl;
     }
-    private String putS3(File uploadFile, String fileName){
-        //외부에서 정적 파일을 읽을 수 있도록 public 읽기 권한으로 put
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile));
-        //업로드 된 파일의 S3 URL 주소 반환
-        return amazonS3Client.getUrl(bucket, fileName).toString();
+
+    private String putS3(File uploadFile, String fileName) {
+        // PutObjectRequest를 생성하고 S3에 파일 업로드
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileName)// 파일 공개 설정
+                .build();
+
+        s3Client.putObject(putObjectRequest, Paths.get(uploadFile.getAbsolutePath()));
+        return s3Client.utilities().getUrl(builder -> builder.bucket(bucket).key(fileName)).toString();
     }
-    private void removeNewFile(File targetFile){
-        // Multipartfile -> file로 전환되면서 로컬에 파일 생성된 것을 삭제
-        if(targetFile.delete()){
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
             System.out.println("파일이 삭제되었습니다.");
-        }else
-        {
+        } else {
             System.out.println("파일이 삭제되지 않았습니다.");
         }
     }
-    private Optional<File> convert(MultipartFile file) throws IOException{
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
         File convertFile = new File(file.getOriginalFilename());
-        if(convertFile.createNewFile()){
-            try (FileOutputStream fileOutputStream = new FileOutputStream(convertFile)){
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(convertFile)) {
                 fileOutputStream.write(file.getBytes());
             }
-
             return Optional.of(convertFile);
         }
         return Optional.empty();
     }
 
-    public void deleteFile(String url){
-        String[] fileName = url.split("/");
-        System.out.println(fileName[3]+fileName[4]);
-        DeleteObjectRequest request = new DeleteObjectRequest(bucket, fileName[3]+"/"+fileName[4]);
-        amazonS3Client.deleteObject(request);
-    }
+    public void deleteFile(String url) {
+        // 파일 URL에서 파일 이름 추출
+        String[] fileNameParts = url.split("/");
+        String fileName = fileNameParts[fileNameParts.length - 2] + "/" + fileNameParts[fileNameParts.length - 1];
 
+        // DeleteObjectRequest를 생성하여 파일 삭제
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileName)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
 }

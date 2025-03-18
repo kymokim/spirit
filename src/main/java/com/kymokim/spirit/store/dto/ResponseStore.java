a68c86d819c8cf0749c9be9ff1c6a89516bb4241
kymokim <kymokim@gmail.com>
2025-03-18 11:31:58 +0900

    Refactor : Store delete logic changed to get handled by Boolean isDeleted.

--- src/main/java/com/kymokim/spirit/store/dto/ResponseStore.java
+++ src/main/java/com/kymokim/spirit/store/dto/ResponseStore.java
@@ -48,6 +48,7 @@ public class ResponseStore {
         private Boolean hasScreen;
         private Boolean isGroupAvailable;
         private Boolean isAlwaysOpen;
+        private Boolean isDeleted;
         private CommonStore.LocationDto locationDto;
         private Set<Category> categories;
         private Set<CommonStore.MainDrinkDto> mainDrinkDtos;
@@ -84,6 +85,7 @@ public class ResponseStore {
                     .hasScreen(store.getHasScreen())
                     .isGroupAvailable(store.getIsGroupAvailable())
                     .isAlwaysOpen(store.getIsAlwaysOpen())
+                    .isDeleted(store.getIsDeleted())
                     .locationDto(CommonStore.LocationDto.toDto(store.getLocation()))
                     .categories(store.getCategories())
                     .mainDrinkDtos(mainDrinkDtos)
@@ -422,6 +424,7 @@ public class ResponseStore {
         private String mainImgUrl;
         private String name;
         private Boolean isAlwaysOpen;
+        private Boolean isDeleted;
         private CommonStore.LocationDto locationDto;
         private Set<Category> categories;
         private Set<CommonStore.OperationInfoDto> operationInfoDtos;
@@ -440,6 +443,7 @@ public class ResponseStore {
                     .mainImgUrl(store.getMainImgUrl())
                     .name(store.getName())
                     .isAlwaysOpen(store.getIsAlwaysOpen())
+                    .isDeleted(store.getIsDeleted())
                     .locationDto(CommonStore.LocationDto.toDto(store.getLocation()))
                     .categories(store.getCategories())
                     .operationInfoDtos(operationInfoDtos)

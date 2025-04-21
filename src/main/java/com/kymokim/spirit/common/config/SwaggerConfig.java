4cfdb31cf7d47c0646952904c4d84ee075807390
kymokim <kymokim@gmail.com>
2025-04-21 12:06:05 +0900

    Fix : SecurityConfiguration fixed.

--- src/main/java/com/kymokim/spirit/common/config/SwaggerConfig.java
+++ src/main/java/com/kymokim/spirit/common/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         Info info = new Info()
                 .version("v1.0.0")
-                .title("Spirit")
+                .title("Spirit api")
                 .description("API");
 
         SecurityScheme securityScheme = new SecurityScheme()

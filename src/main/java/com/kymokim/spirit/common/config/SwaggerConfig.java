6f176f8e5cf639d7137a8cca17f48994ddb9f033
kymokim <kymokim@gmail.com>
2025-04-21 12:18:00 +0900

    Fix : CD pipeline issue solved.

--- src/main/java/com/kymokim/spirit/common/config/SwaggerConfig.java
+++ src/main/java/com/kymokim/spirit/common/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         Info info = new Info()
                 .version("v1.0.0")
-                .title("TEST")
+                .title("Spirit")
                 .description("API");
 
         SecurityScheme securityScheme = new SecurityScheme()

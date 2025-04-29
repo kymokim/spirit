25429c56da8b07d100b40abea9d2ec17f426412d
Junseo Yun <yunjunseo@naver.com>
2025-04-29 12:11:04 +0900

    Feat : 특정 가게 신고 리스트 조회

--- src/main/java/com/kymokim/spirit/report/service/ReportService.java
+++ src/main/java/com/kymokim/spirit/report/service/ReportService.java
@@ -5,6 +5,7 @@ import com.kymokim.spirit.auth.exception.AuthErrorCode;
 import com.kymokim.spirit.auth.repository.AuthRepository;
 import com.kymokim.spirit.common.exception.CustomException;
 import com.kymokim.spirit.common.service.TransactionRetryUtil;
+import com.kymokim.spirit.menu.dto.ResponseMenu;
 import com.kymokim.spirit.report.dto.RequestReport;
 import com.kymokim.spirit.report.dto.ResponseReport;
 import com.kymokim.spirit.report.entity.Report;
@@ -19,13 +20,15 @@ import com.kymokim.spirit.review.repository.ReviewRepository;
 import com.kymokim.spirit.store.entity.Store;
 import com.kymokim.spirit.store.exception.StoreErrorCode;
 import com.kymokim.spirit.store.repository.StoreRepository;
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -66,7 +69,7 @@ public class ReportService {
         reportRepository.save(report);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ResponseReport.StoreReportListDto> getStoreReports(Pageable pageable, ReportStatus reportStatus) {
         return TransactionRetryUtil.executeWithRetry(() -> {
             Page<Report> reportPage = reportRepository.findAllByReportTargetAndReportStatusOrderByReportedAtAsc(
@@ -80,7 +83,8 @@ public class ReportService {
         }, 3);
     }
 
-    @Transactional
+
+    @Transactional(readOnly = true)
     public Page<ResponseReport.StoreReportListDto> getPriorityStoreReports(Pageable pageable, ReportStatus reportStatus) {
         List<ReportReason> priorityReasons = List.of(
                 ReportReason.INAPPROPRIATE_LANGUAGE,
@@ -100,7 +104,18 @@ public class ReportService {
         }, 3);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
+    public List<ResponseReport.StoreReportDto> getReportsByStoreId(Long storeId) {
+        List<Report> reports = reportRepository.findAllByReportTargetAndTargetId(ReportTarget.STORE, storeId);
+
+        List<ResponseReport.StoreReportDto> reportList = new ArrayList<>();
+        reports.forEach(report -> reportList.add(ResponseReport.StoreReportDto.toDto(report)));
+
+        return reportList;
+
+    }
+
+    @Transactional(readOnly = true)
     public Page<ResponseReport.ReviewReportListDto> getReviewReports(Pageable pageable, ReportStatus reportStatus) {
         return TransactionRetryUtil.executeWithRetry(() -> {
             Page<Report> reportPage = reportRepository.findAllByReportTargetAndReportStatusOrderByReportedAtAsc(
@@ -114,25 +129,6 @@ public class ReportService {
         }, 3);
     }
 
-    @Transactional
-    public Page<ResponseReport.ReviewReportListDto> getPriorityReviewReports(Pageable pageable, ReportStatus reportStatus) {
-        List<ReportReason> priorityReasons = List.of(
-                ReportReason.INAPPROPRIATE_LANGUAGE,
-                ReportReason.INAPPROPRIATE_PHOTO,
-                ReportReason.VIOLATION_OF_GUIDELINES
-        );
-        return TransactionRetryUtil.executeWithRetry(() -> {
-            Page<Report> reportPage = reportRepository.findAllByReportTargetAndReportStatusAndReportReasonInOrderByReportedAtAsc(
-                    ReportTarget.REVIEW, reportStatus, priorityReasons, pageable);
-
-            return reportPage.map(report ->
-                    ResponseReport.ReviewReportListDto.toDto(
-                            report,
-                            resolveReview(report.getTargetId())
-                    )
-            );
-        }, 3);
-    }
 
     @Transactional
     public void handleReport(ReportStatus reportStatus, Long reportId) {

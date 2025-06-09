20487ed184562b2178270aedf251c9366c42ab4c
kymokim <kymokim@gmail.com>
2025-06-10 02:02:47 +0900

    Feat : app link, universal link certificate added.

--- src/main/java/com/kymokim/spirit/store/controller/StoreShareController.java
+++ src/main/java/com/kymokim/spirit/store/controller/StoreShareController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,5 +46,38 @@ public class StoreShareController {
         res.setContentType(MediaType.TEXT_HTML_VALUE);
         res.setCharacterEncoding(StandardCharsets.UTF_8.name());
         res.getWriter().write(shareService.iosStoreHtml());
+    }
+
+    @GetMapping(value = "/.well-known/assetlinks.json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAndroidAssetLinks() {
+        return """
+                [
+                  {
+                    "relation": ["delegate_permission/common.handle_all_urls"],
+                    "target": {
+                      "namespace": "android_app",
+                      "package_name": "com.spiritfront",
+                      "sha256_cert_fingerprints": [
+                        "FD:D5:B2:A3:27:63:F3:94:19:D7:6D:A2:02:A6:74:AF:FD:31:28:C8:DD:D5:AB:11:D7:87:7E:5A:AC:30:44:9B"
+                      ]
+                    }
+                  }
+                ]
+                """;
+    }
+
+    @GetMapping(value = "/.well-known/apple-app-site-association", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> getAppleAppSiteAssociation() {
+        return Map.of(
+                "applinks", Map.of(
+                        "apps", List.of(),
+                        "details", List.of(
+                                Map.of(
+                                        "appID", "B6Y8UPNX32.com.spirit.drinkToday",
+                                        "paths", List.of("/ul/store/*")
+                                )
+                        )
+                )
+        );
     }
 }

81b60a08cb25f5d58b820211fe637267c3187ee5
kymokim <kymokim@gmail.com>
2025-06-19 15:55:52 +0900

    Feat : Menu sort order logic created.

--- src/main/java/com/kymokim/spirit/menu/repository/MenuRepository.java
+++ src/main/java/com/kymokim/spirit/menu/repository/MenuRepository.java
@@ -2,9 +2,15 @@ package com.kymokim.spirit.menu.repository;
 
 import com.kymokim.spirit.menu.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    @Query("SELECT MAX(m.sortOrder) FROM Menu m WHERE m.store.id = :storeId")
+    Optional<Integer> findMaxSortOrderByStoreId(@Param("storeId") Long storeId);
+
 }

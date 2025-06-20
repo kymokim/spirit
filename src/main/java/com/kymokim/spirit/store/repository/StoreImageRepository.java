c86e1d01d7ca92828ae787d8fd8104ba84edd763
kymokim <kymokim@gmail.com>
2025-06-20 14:45:35 +0900

    Feat : store image sort order logic created.

--- src/main/java/com/kymokim/spirit/store/repository/StoreImageRepository.java
+++ src/main/java/com/kymokim/spirit/store/repository/StoreImageRepository.java
@@ -4,8 +4,10 @@ import com.kymokim.spirit.store.entity.Store;
 import com.kymokim.spirit.store.entity.StoreImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreImageRepository extends JpaRepository<StoreImage, Long> {
     Optional<StoreImage> findByUrl(String url);
+    List<StoreImage> findAllByUrlIn(List<String> urlList);
 }

b37204da8199945bd695308243cc6dd52b1bb9b8
kymokim <kymokim@gmail.com>
2025-07-21 17:13:36 +0900

    Feat : Store view log domain, logic changed.

---
+++ src/main/java/com/kymokim/spirit/log/controller/LogController.java
@@ -0,0 +1,41 @@
+package com.kymokim.spirit.log.controller;
+
+import com.kymokim.spirit.auth.entity.Gender;
+import com.kymokim.spirit.common.dto.ResponseDto;
+import com.kymokim.spirit.log.dto.ResponseLog;
+import com.kymokim.spirit.log.service.LogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Log API")
+@RestController
+@RequestMapping("/api/log")
+@RequiredArgsConstructor
+public class LogController {
+
+    private final LogService logService;
+
+    @Operation(summary = "매장 조회 로그 통계", description = "groupBy field : ageGroup, gender, role")
+    @GetMapping("/store-view/stats")
+    public ResponseEntity<ResponseDto> getAccessLogStats(
+            @RequestParam Long storeId,
+            @RequestParam String period,
+            @RequestParam(required = false) Gender gender,
+            @RequestParam(required = false) List<String> ageGroup,
+            @RequestParam List<String> groupBy,
+            @RequestParam String showBy
+    ) {
+        List<ResponseLog.StoreViewLogStatListDto> dtoList =  logService.getStoreViewLogStats(storeId, period, gender, ageGroup, groupBy, showBy);
+        ResponseDto responseDto = ResponseDto.builder()
+                .message("Store view log stats retrieved successfully.")
+                .data(dtoList)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+}
---
+++ src/main/java/com/kymokim/spirit/log/repository/StoreViewLogRepository.java
@@ -0,0 +1,10 @@
+package com.kymokim.spirit.log.repository;
+
+import com.kymokim.spirit.log.entity.StoreViewLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface StoreViewLogRepository extends JpaRepository<StoreViewLog, Long>, StoreViewLogRepositoryCustom {
+    boolean existsByUserIdAndStoreIdAndViewDate(Long userId, Long storeId, LocalDate viewDate);
+}

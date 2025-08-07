1e27d04a2156f79d3175224ee69cd4de4e2a6af0
kymokim <kymokim@gmail.com>
2025-08-08 03:19:24 +0900

    Fix: test settings changed.

--- src/main/java/com/kymokim/spirit/SpiritApplication.java
+++ src/main/java/com/kymokim/spirit/SpiritApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableScheduling
 public class SpiritApplication {
 	static {
